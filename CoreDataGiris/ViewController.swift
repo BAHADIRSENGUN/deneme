45bc869c565d7d2fc37ded32937a2b9f2f24c9ce
Bircan Sezgin <bircansezgin@outlook.com.tr>
2023-03-12 15:03:14 +0200

    yuklendi

--- CoreDataGiris/ViewController.swift
+++ CoreDataGiris/ViewController.swift
@@ -6,14 +6,50 @@
 //
 
 import UIKit
+import CoreData
+
+// Tum islemler Bununla yapiliyor.
+let appDelegete = UIApplication.shared.delegate as! AppDelegate
 
 class ViewController: UIViewController {
-
+// data kisminda olusturgumuz(modelini) veri tabanini buraya yerlestiriyoruz.
+//Cogunlukla -  Veri kaydetme Okumada gereklidir.
+// Veri cekmede de kullanilir
+    let context = appDelegete.persistentContainer.viewContext
+    
+    @IBOutlet weak var userNameTextField: UITextField!
+    var kisilerListe = [Kisiler]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        veriKaydi()
+  
+        
     }
-
+        // Veri kaydi
+    func veriKaydi(){
+    // Kisiler tabosundan bir nesene olusutruyoruz!
+        let kisi = Kisiler(context: context)
+        kisi.kisi_ad = userNameTextField.text!
+        kisi.kisi_yas = 0
+    // olusturulan isim ve yas nesnelerini veri tabananina kaydetmek icin bunu kullaniyoruz.
+        appDelegete.saveContext()
+    }
+    
+    func veriOkuma(){
+        
+        
+        do{
+            kisilerListe = try context.fetch(Kisiler.fetchRequest())
+        }catch{
+            print("veri okurken hata olustu")
+        }
+        
+        for k in kisilerListe{
+            print("ad = \(k.kisi_ad!) yas = \(k.kisi_yas)")
+        }
+    }
 
 }
 
